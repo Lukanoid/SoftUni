33caec56ac2db6ca4ed7bda50afb16172d6cf955
Lukanoid <vasil.stamboliiski1@gmail.com>
2023-09-25 22:00:00 +0300

    Uploaded part of my exercises for Data Types and Variables

---
+++ Fundamentals/Data Types and Variables - Lab/ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double kilometers = meters / 1000;
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
---
+++ Fundamentals/Data Types and Variables - Lab/PoundsToDollars/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PoundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31m;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
