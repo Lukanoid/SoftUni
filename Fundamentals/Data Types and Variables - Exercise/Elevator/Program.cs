73eba6bf5546a1f7883a1a3512fca8245e50b82e
Lukanoid <vasil.stamboliiski1@gmail.com>
2023-09-28 14:57:03 +0300

    Added folder for my Data types and Variables Exercises and add some of my tasks

---
+++ Fundamentals/Data Types and Variables - Exercise/Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)numberOfPeople / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
---
+++ Fundamentals/Data Types and Variables - Exercise/Integer Operations/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            int output = firstNum + secondNum;
+            output /= thirdNum;
+            output *= fourthNum;
+
+            Console.WriteLine(output);
+        }
+    }
+}
