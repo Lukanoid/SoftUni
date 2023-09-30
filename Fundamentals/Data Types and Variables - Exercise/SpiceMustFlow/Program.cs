1451e55ca49660dc43a041fb2e181217f63fcebb
Lukanoid <vasil.stamboliiski1@gmail.com>
2023-09-30 19:31:06 +0300

    Uploading 2 out of 3 of my "not included in finals score exercise"

---
+++ Fundamentals/Data Types and Variables - Exercise/SpiceMustFlow/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long startingYield = long.Parse(Console.ReadLine());
+            long dayCounter = 0;
+            long workersConsumption = 26;
+            long totalSpiceExtracted = 0;
+
+            while (true)
+            {
+                
+                if (startingYield < 100)
+                {
+                    break;
+                }               
+                totalSpiceExtracted += startingYield - workersConsumption;
+                dayCounter++;
+                startingYield -= 10;
+            }
+            if (totalSpiceExtracted >= 26)
+            {
+                totalSpiceExtracted-= 26;
+            }
+            Console.WriteLine(dayCounter);
+            Console.WriteLine(totalSpiceExtracted);
+        }
+    }
+}
