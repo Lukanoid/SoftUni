3857d31b86e721271c9039afc4444b32989a92cd
Lukanoid <vasil.stamboliiski1@gmail.com>
2023-10-22 01:43:06 +0300

    Added folder for Exam preparation number 1 and 2 out of 3 tasks included

---
+++ Fundamentals/ExamPreparation1/Problem1-CounterStrike/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Problem1_CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            int winsCount = 0;
+            bool isEnergyEnough = true;
+
+            string input = Console.ReadLine();
+
+            while (input != "End of battle")
+            {
+                
+                int distance = int.Parse(input);
+
+                if (energy - distance < 0)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {winsCount} won battles and {energy} energy");
+                    isEnergyEnough = false;
+                    break;
+                }
+
+                energy -= distance;
+                winsCount++;
+
+                if (winsCount % 3 == 0)
+                {
+                    energy += winsCount;
+                }
+                input = Console.ReadLine();
+            }
+
+            if (isEnergyEnough)
+            {
+                Console.WriteLine($"Won battles: {winsCount}. Energy left: {energy}");
+            }
+
+        }
+    }
+}
---
+++ Fundamentals/ExamPreparation1/Problem2-TreasureHunt/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.Security;
+
+namespace Problem2_TreasureHunt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> initialLoot = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Yohoho!")
+            {
+                string[] command = input.Split();
+
+                if (command[0] == "Loot")
+                {
+                    for (int i = 1; i < command.Length; i++)
+                    {
+                        if (!initialLoot.Contains(command[i]))
+                        {
+                            initialLoot.Insert(0, command[i]);
+                        }
+                    }
+                }
+                else if (command[0] == "Drop")
+                {
+                    int index = int.Parse(command[1]);
+                    if (index >= 0 && index < initialLoot.Count)
+                    {
+                        string item = initialLoot[index];
+                        initialLoot.RemoveAt(index);
+                        initialLoot.Add(item);
+                    }
+                }
+                else if (command[0] == "Steal")
+                {
+                    int count = int.Parse(command[1]);
+
+                    List<string> removedLoot = new List<string>();
+
+                    for (int i = initialLoot.Count - 1; i >= initialLoot.Count - count; i--)
+                    {
+                        if (i < 0)
+                        {
+                            break;
+                        }
+                        removedLoot.Insert(0 ,initialLoot[i]);
+                    }
+
+                    for (int i = 0; i < removedLoot.Count; i++)
+                    {
+                        string currentItem = removedLoot[i];
+                        initialLoot.Remove(currentItem);
+                    }
+
+                    Console.WriteLine(string.Join(", ", removedLoot));
+                }
+                input = Console.ReadLine();
+            }
+
+            if (initialLoot.Count > 0)
+            {
+                double average = 0;
+                double totalLenght = 0;
+
+                foreach (var item in initialLoot)
+                {
+                    totalLenght += item.Length;
+                }
+
+                average = totalLenght / initialLoot.Count;
+
+                Console.WriteLine($"Average treasure gain: {average:f2} pirate credits.");
+            }
+            else
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+        }
+    }
+}
