8ff47aa084ba10b2ed8060f73a45c02ba4a1b799
Lukanoid <vasil.stamboliiski1@gmail.com>
2023-11-10 02:06:52 +0200

    Added rest of my exercises included 7-Vehicle Catalogues which gives errors

---
+++ Fundamentals/ObjectsАndClasses-Lab/StoreBoxes/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace StoreBoxes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Box> boxes = new List<Box>();
+
+            while (input != "end")
+            {
+                string[] receivedData = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string serailNumber = receivedData[0];
+                string itemName = receivedData[1];
+                int itemQuantity = int.Parse(receivedData[2]);
+                decimal itemPrice = decimal.Parse(receivedData[3]);
+
+                Item item = new Item()
+                {
+                    Name = itemName,
+                    Price = itemPrice
+                };
+
+                Box box = new Box()
+                {
+                    SerialNumber = serailNumber,
+                    Item = item,
+                    ItemQuantity = itemQuantity,
+                    PriceForBox = itemQuantity * itemPrice
+                };
+
+                boxes.Add(box);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var box in boxes.OrderByDescending(x => x.PriceForBox))
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceForBox:F2}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// {boxSerialNumber}
+    /// 
+    /// -- {boxItemName} – ${boxItemPrice}: {boxItemQuantity}
+    /// 
+    /// -- ${boxPrice}
+    /// </summary>
+
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    public class Box
+    {
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal PriceForBox { get; set; }
+
+    }
+}
---
+++ Fundamentals/ObjectsАndClasses-Lab/VehicleCatalogue/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace VehicleCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalogue catalogue = new Catalogue();
+
+            string commandInfo = Console.ReadLine();
+
+            while (commandInfo != "end")
+            {
+                string[] vehicleInfo = commandInfo.Split("/");
+
+                string type = vehicleInfo[0];
+                string make = vehicleInfo[1];
+                string model = vehicleInfo[2];
+                int value = int.Parse(vehicleInfo[3]);
+
+                if (type == "Car")
+                {
+                    Car car = new Car();
+                    car.Brand = make;
+                    car.Model = model;
+                    car.HorsePower = value;
+
+                    catalogue.Cars.Add(car);
+                }
+                else
+                {
+                    Truck truck = new Truck();
+                    truck.Brand = make;
+                    truck.Model = model;
+                    truck.Weight = value;
+
+                    catalogue.Trucks.Add(truck);
+                }
+
+                commandInfo = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Cars:");
+            foreach (var car in catalogue.Cars.OrderBy(x => x.Brand))
+            {
+                Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+            }
+
+            Console.WriteLine("Trucks:");
+            foreach (var truck in catalogue.Trucks.OrderBy(x => x.Brand))
+            {
+                Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+            }
+        }
+    }
+
+    public class Car
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+    }
+
+    public class Truck
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class Catalogue
+    {
+        public Catalogue()
+        {
+            Trucks = new List<Truck>();
+            Cars = new List<Car>();
+        }
+
+        public List<Car> Cars { get; set; }
+
+        public List<Truck> Trucks { get; set; }
+    }
+
+}
