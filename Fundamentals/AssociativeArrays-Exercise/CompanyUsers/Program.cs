d27c1f7cabb482383e7bc488cb46bae94b86fa71
Lukanoid <vasil.stamboliiski1@gmail.com>
2023-11-23 18:02:18 +0200

    Added rest of the exercises
    Added exercise 6 and 7 for Associative Arrays Exercise

---
+++ Fundamentals/AssociativeArrays-Exercise/CompanyUsers/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> employees = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] data = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = data[0];
+                string employeeId = data[1];
+
+                if (!employees.ContainsKey(companyName))
+                {
+                    employees.Add(companyName, new List<string>());
+                    employees[companyName].Add(employeeId);
+                }
+                else
+                {
+                    if (!employees[companyName].Contains(employeeId))
+                    {
+                        employees[companyName].Add(employeeId);
+                    }
+                }
+            }
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Key);
+                foreach (var human in employee.Value)
+                {
+                    Console.WriteLine($"-- {human}");
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/AssociativeArrays-Exercise/StudentAcademy/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, new List<double>());
+                    students[studentName].Add(grade);
+                }
+                else
+                {
+                    students[studentName].Add(grade);
+                }
+            }
+
+            foreach (var student in students)
+            {
+                double average = student.Value.Average();
+
+                if (average >= 4.5)
+                {
+                    Console.WriteLine($"{student.Key} -> {average:f2}");
+                }
+            }
+        }
+    }
+}
