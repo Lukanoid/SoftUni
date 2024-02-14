1b705cbe7cb09cc1869f8992844dc589feaec202
Lukanoid <vasil.stamboliiski1@gmail.com>
2024-02-14 21:55:20 +0200

    Added folder for CustomSoftUniLinkedList
    Added folder for CustomSoftUniLinkedList workshop exercise included

---
+++ Advanced/CustomDoublyLinkedList/CustomDoublyLinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomDoublyLinkedList;
+
+public class Node
+{
+    public Node(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; set; }
+
+    public  Node Next { get; set; }
+
+    public Node Previous { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Previous?.Value}<-{Value}->{Next?.Value}";
+    }
+}
---
+++ Advanced/CustomDoublyLinkedList/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomDoublyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
