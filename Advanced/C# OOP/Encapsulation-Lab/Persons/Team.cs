d2ba8b5bb68098a71b4e171c07064a2ea7022136
Lukanoid <vasil.stamboliiski1@gmail.com>
2024-03-09 14:15:59 +0200

    Added folder for Encapsulation - Lab
    Added folder for Encapsulation - Lab and all the exercises included with 100/100

---
+++ Advanced/C# OOP/Encapsulation-Lab/Persons/Team.cs
@@ -0,0 +1,50 @@
+﻿namespace PersonsInfo;
+
+public class Team
+{
+	private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(string name)
+    {
+        Name = name;
+        firstTeam = new List<Person>();
+        reserveTeam = new List<Person>();
+    }
+
+	public string Name
+	{
+        get
+        {
+            return name;
+        }
+        private set
+        {
+            name = value;
+        }
+	}
+
+
+    public IReadOnlyCollection<Person> FirstTeam
+    {
+        get => this.firstTeam.AsReadOnly();
+    }
+
+    public IReadOnlyCollection<Person> ReserveTeam
+    {
+        get => this.reserveTeam.AsReadOnly();
+    }
+
+    public void AddPlayer(Person person)
+    {
+        if (person.Age < 40)
+        {
+            firstTeam.Add(person);
+        }
+        else
+        {
+            reserveTeam.Add(person);
+        }
+    }
+}
